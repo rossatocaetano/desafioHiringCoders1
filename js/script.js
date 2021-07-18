0c8e4e3d014e0ee095ca75e25c8766454b954d75
Raphael Rossato Caetano <rossato.caetano@gmail.com>
2021-07-18 20:51:37 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+
+    let data = {
+        nome,
+        email,
+    }
+    
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData);
+    console.log(nome, email);
+    alert('Cadastro realizado com sucesso!');
+})
